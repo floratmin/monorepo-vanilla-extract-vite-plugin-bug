88d9d93063c223cbb66063cad3238b36cebd9ece
Florian Koch <florat.min@gmail.com>
2024-11-05 10:04:23 +0100

    build works

--- apps/page/app/src/components/Counter.tsx
+++ apps/page/app/src/components/Counter.tsx
@@ -1,12 +1,12 @@
 import { createSignal } from "solid-js";
 import {increment} from './Counter.css.ts';
-import {Button} from '@workspace/app-elements';
+// import {Button} from '@workspace/app-elements';
 
 export default function Counter() {
   const [count, setCount] = createSignal(0);
   return (
     <>
-      <Button>Imported styled button</Button>
+      {/*<Button>Imported styled button</Button>*/}
       <button class={increment} onClick={() => setCount(count() + 1)} type="button">
         Clicks: {count()}
       </button>
