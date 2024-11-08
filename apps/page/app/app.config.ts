e4b7e6c207917209e9134b280d4451d7e2c678d3
Florian Koch <florat.min@gmail.com>
2024-11-08 10:35:12 +0100

    fix vanilla css plugin

--- apps/page/app/app.config.ts
+++ apps/page/app/app.config.ts
@@ -1,7 +1,21 @@
 import {defineConfig} from "@solidjs/start/config";
 import {nxViteTsPaths} from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 import {vanillaExtractPlugin} from '@vanilla-extract/vite-plugin';
+import {Plugin} from 'vite';
 
+function fixVanillaExtract(): Plugin {
+  const basePath = `${process.cwd()}/`;
+  return {
+    name: 'fix-vanilla-extract',
+    renderChunk(code) {
+      console.log(process.cwd())
+      if (code.includes(basePath)) {
+        return code.replaceAll(basePath, '')
+      }
+      return null;
+    },
+  };
+}
 export default defineConfig({
   server: {
     prerender: {
@@ -15,6 +29,7 @@ export default defineConfig({
     plugins: [
       nxViteTsPaths(),
       vanillaExtractPlugin(),
+      fixVanillaExtract(),
     ],
     optimizeDeps: {
       exclude: [
